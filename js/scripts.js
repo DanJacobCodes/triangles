63de063ea55179fbb4ce30195519b51bf3831b51
Dan Jacob and jason Lazzuri <jason@>
2017-03-23 16:51:18 -0700

    finished

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,17 @@ $(document).ready(function() {
     var side2Input = parseInt($("#side2").val());
     var side3Input = parseInt($("#side3").val());
 
-    if((side1Input || side2Input) === side3Input){
+    if((side1Input === side2Input) && (side2Input === side3Input)){
       alert("This is an equilateral triangle");
-    } else if (side1Input === side2Input != side3Input){
-      alert("This is an isosceles");
-    } else if (side1Input != side2Input != side3Input){
-      alert("This is a scalene triangle");
     }
 
+    else if (((side1Input === side2Input) || (side2Input === side3Input) || (side3Input === side1Input)) && ((side1Input + side2Input > side3Input) && (side2Input + side3Input > side1Input) && (side1Input + side3Input > side2Input))){
+      alert("This is an isosceles");
+    } else if (((side1Input !== side2Input) && (side2Input!==side3Input) && (side3Input!==side1Input)) && ((side1Input + side2Input > side3Input) && (side2Input + side3Input > side1Input) && (side1Input + side3Input > side2Input))){
+      alert("This is a scalene triangle");
+    }else{
+      alert("This is not a triangle!!!");
+    }
    event.preventDefault()
   });
 });
