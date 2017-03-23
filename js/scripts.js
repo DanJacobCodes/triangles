fbd6ac645f6bb0e445456dedf786fe428d78872c
Dan Jacob and jason Lazzuri <dan@>
2017-03-23 16:03:39 -0700

    added variables to if else, and parenthesis

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
+  $("#triangle").submit(function(event) {
 
-}
+    var side1Input = parseInt($("#side1").val());
+    var side2Input = parseInt($("#side2").val());
+    var side3Input = parseInt($("#side3").val());
+
+    if((side1Input || side2Input) === side3Input){
+      alert("This is an equilateral triangle");
+    } else if (side1Input === side2Input != side3Input){
+      alert("This is an isosceles");
+    } else if (side1Input != side2Input != side3Input){
+      alert("This is a scalene triangle");
+    }
+
+   event.preventDefault()
+  });
+});
